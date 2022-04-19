4298a732ba6239776a9fdddbafae97e8e2ea94a8
Rafet Abdalgalil <rafetalani@gmail.com>
2022-04-18 17:27:08 -0700

    added note

--- src/api/index.ts
+++ src/api/index.ts
@@ -8,4 +8,5 @@ export default router;
 // all cars routes will be prefixed with /api/cars and will be handled by the carsRoute router
 router.use('/cars', carsRoute);
 
+// all bids routes will be prefixed with /api/bids and will be handled by the bidsRoute router
 router.use('/bids', bidsRoute);
