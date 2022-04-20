5105e03a252214bbec6b5efc13dfb48b7fe0d60d
Rafet Abdalgalil <rafetalani@gmail.com>
2022-04-20 13:24:42 -0700

    added s3.ts file

---
+++ src/api/controllers/s3.ts
@@ -0,0 +1,27 @@
+import aws from 'aws-sdk';
+
+const region = "us-west-2"
+const bucketName = "benzbid-cars-images"
+const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+
+const s3 = new aws.S3({
+    region,
+    accessKeyId,
+    secretAccessKey,
+    signatureVersion: 'v4'
+  })
+
+  
+
+export const generateUploadURL = async() => {
+    const imageName = Date.now().toString();
+
+    const params = ({
+        Bucket: bucketName,
+        Key: imageName,
+        Expires: 60,
+    });
+    const uploadUrl = await s3.getSignedUrlPromise('putObject', params);
+    return uploadUrl;
+}
